6093163a67860e52dc177daba92a9d51822b5ed4
seroking <baridmohamedamine004@gmail.com>
2023-04-24 07:39:14 -0700

    main.h

--- main.h
+++ main.h
@@ -1,5 +1,25 @@
+								/* Mohamed Amine Barid - yassir haouane */
+
+
 #ifndef MAIN_H
 #define MAIN_H
+
+/**
+ * struct conversion - struct to choose symbol
+ *@letter : the symbol after the "%" (c / s)
+ *@f : pointer function
+ */
+
+typedef struct conversion
+{
+
+	int (*f)(va_list);
+	char *letter;
+
+}convert_t;
+
+
+								/* STANDARS LIBRARIES */
 #include <stdarg.h>
 #include <unistd.h>
 #include <limits.h>
@@ -7,13 +27,14 @@
 #include <stddef.h>
 #include <string.h>
 #include <math.h>
-#define BUFF_SIZE 1024
+
+								/* FUNCTIONS FOR TASK 0 */
+
 int _printf(const charformat, ...);
-int _check(va_list list, char c);
+
+
+
+
 int _putchar(char c);
-int _strlen(char s);
-int _print_str(charstr);
-int _print_bin(unsigned int n);
-int _print_unsigned_int(unsigned int n, int base, char *digit);
-int _print_int(int n);
+
 #endif
