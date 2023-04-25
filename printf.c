803070073a73b6b40f5ac16710f9d1247d4ad67e
YHaouane <yassirhaouane8@gmail.com>
2023-04-25 12:19:33 -0700

    please make it work

---
+++ printf.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/* BY YASSIR HAOUANE AND MOHAMED AMINE BARID*/
+/**
+ * _printf - our printf version for alx PLD
+ * @format: format
+ * Return: size_f
+ */
+
+int _printf(const char *format, ...)
+{
+	int size_f = 0;
+	va_list args;
+
+	if (format == NULL)
+	{
+		return (-1);
+	}
+
+	size_f = _strlen(format);
+	if (size_f <= 0)
+		return (0);
+
+	va_start(args, format);
+	size_f = checker(format, args);
+
+	_putchar(-1);
+	va_end(args);
+
+	return (size_f);
+}
